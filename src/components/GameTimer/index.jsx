71800f3b40f2e56e7c793b9606a8057a4dcd3314
Piyush <piyushguptaa2z123@gmail.com>
2022-01-03 14:13:09 +0530

    quiz timer is completed

--- src/components/GameTimer/index.jsx
+++ src/components/GameTimer/index.jsx
@@ -1,8 +1,33 @@
+import { useEffect, useState } from "react";
 import TimerStyles from "./GameTimer.module.css";
 
 const GameTimer = () => {
-  const min = 10;
-  const sec = 1;
+  const [sec, setSec] = useState("00");
+  const [min, setMin] = useState("00");
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const secondCounter = counter % 60;
+      const minuteCounter = Math.floor(counter / 60);
+
+      const computedSecond =
+        String(secondCounter).length === 1
+          ? `0${secondCounter}`
+          : secondCounter;
+      const computedMinute =
+        String(minuteCounter).length === 1
+          ? `0${minuteCounter}`
+          : minuteCounter;
+
+      setSec(computedSecond);
+      setMin(computedMinute);
+      setCounter((counter) => counter + 1);
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, [counter]);
+
   return (
     <div className={TimerStyles.timer}>
       <div className="min">
